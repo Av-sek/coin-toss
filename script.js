43a5887572ad493e418b4636cfd859866d76e8d0
Av-sek <66995184+Av-sek@users.noreply.github.com>
2021-06-08 09:13:40 +0545

    Update script.js

--- script.js
+++ script.js
@@ -1,30 +1,38 @@
 var button= document.getElementById("button");
+var value=button;
 var luck=document.getElementById("luck")
 function heads()
 {
     document.getElementById("cointoss").src="heads.jpg";
     luck.innerHTML="Heads";
+    value.innerHTML="Flip again";
+    button=value;
 }
 function tails()
 {
     document.getElementById("cointoss").src="tails.png";
     luck.innerHTML="Tails";
+    value.innerHTML="Flip again";
+    button=value;
 }
 function flipping()
 {
+    console.log("called")
     document.getElementById("cointoss").src="coin.gif";
 }
 function myfunction()
 {
-    luck.innerHTML="Flipping";
-    var random=Math.floor(Math.random()*9+1);
+    button.innerHTML="flipping";
+    button=null;
+    var random=Math.ceil(Math.random()*9+1);
+    console.log(random);
     flipping();
 
     if(random%2==0)
     {
         window.setTimeout(() => {
             heads()
-        }, 4000);
+        }, 5000);
     }
     else
     {
